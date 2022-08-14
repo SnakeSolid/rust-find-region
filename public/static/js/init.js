a322ea6b7e585e4694d7511cbb5791c24294e751
Shabanov Anton <snakesolid@ngs.ru>
2022-08-14 20:54:41 +0300

    updated js dependencies

--- public/static/js/init.js
+++ public/static/js/init.js
@@ -3,7 +3,7 @@
 requirejs.config({
 	baseUrl: "/static/js",
 	paths: {
-		knockout: ["https://cdnjs.cloudflare.com/ajax/libs/knockout/3.5.0/knockout-min", "lib/knockout-min"],
+		knockout: ["https://cdnjs.cloudflare.com/ajax/libs/knockout/3.5.1/knockout-min", "lib/knockout-min"],
 		reqwest: ["https://cdnjs.cloudflare.com/ajax/libs/reqwest/2.0.5/reqwest.min", "lib/reqwest.min"],
 		semantic: ["https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min", "lib/semantic.min"],
 		text: ["https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min", "lib/text.min"],
